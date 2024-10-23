9e3f99332402dcc8b244157f3a5b6f2a418b8463
neves-c-leonardo <neves.c.leonardo@gmail.com>
2024-10-22 23:05:18 -0300

    Melhorando calculadora

--- projetos/js/script.js
+++ projetos/js/script.js
@@ -1,36 +1,17 @@
+let campoCalculo = "";
+let resultado = 0;
 
-function calcular(operador) {
+function digitar(digito) {
+    campoCalculo += `${digito} `;
+    document.getElementById("campoCalculo").value = campoCalculo;
+}
 
-    let num1 = document.getElementById("num1").value ;
-    let num2 = document.getElementById("num2").value;
-    let resultado = 0;
+function calcular() {
+    resultado = eval(campoCalculo);
+    document.getElementById("campoCalculo").value = resultado;
+}
 
-    // Protegendo de Nulidade
-    num1 = num1 == "" ? 0 : parseFloat(num1);
-    num2 = num2 == "" ? 0 : parseFloat(num2);
-
-    if(operador === '+') {
-        resultado = num1 + num2;
-    } else if(operador === '-'){
-        resultado = num1 - num2;
-    } else if(operador === 'x'){
-        resultado = num1 * num2;
-    } else if(operador === '/'){
-        if(num2 === 0) {
-            document.getElementById("resultado").innerHTML = 
-                "Divisão por Zero não permitida!";
-            return;
-        }
-        resultado = parseFloat((num1 / num2).toFixed(4));
-    } else if(operador === 'potencia') {
-        resultado = num1 ** num2;
-    } else if(operador === 'c') {
-        document.getElementById("num1").value = "";
-        document.getElementById("num2").value = "";
-        document.getElementById("resultado").innerHTML = 0;
-
-        return;
-    }
-
-    document.getElementById("resultado").innerHTML = resultado;
+function limpar() {
+    campoCalculo = "";
+    document.getElementById("campoCalculo").value = "";
 }
